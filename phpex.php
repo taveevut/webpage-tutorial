6e5b32df3ead314f33650f1a22fd1c57a9413e3d
taveevut <nakomah.web@gmail.com>
2021-07-24 13:55:17 +0700

    fix: ตัวอย่างการเขียนโปรแกรมด้วยภาษา php

---
+++ phpex.php
@@ -0,0 +1,17 @@
+<?php 
+
+$score = 40;
+
+if($score >= 80){
+   echo "Grad: A";
+}elseif($score >= 70){
+   echo "Grad: B";
+}elseif($score >= 60){
+   echo "Grad: C";
+}elseif($score >= 50){
+   echo "Grad: D";
+}else{
+   echo "Grad: E";
+}
+
+?>
